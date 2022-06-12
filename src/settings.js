a7e6b15156469586268391241366efbb6f6a6589
晨曦 <c15293452071@163.com>
2022-06-12 15:54:02 +0800

    人力资源项目初始化

---
+++ src/settings.js
@@ -0,0 +1,17 @@
+module.exports = {
+
+  title: 'Vue Admin Template',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: false,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  // 是否显示左侧logo
+  sidebarLogo: true
+}
---
+++ src/store/modules/user.js
@@ -0,0 +1,13 @@
+/**
+ * 存储用户信息,token、个人信息
+ */
+export default {
+  // 模块化加锁
+  namespaced: true,
+  // 1.定义变量
+  state: {},
+  // 2.修改变量（同步）
+  mutations: {},
+  // 3.调用后台接口发送请求（异步）
+  actions: {}
+}

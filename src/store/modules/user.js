9b4e56f62abe977e66f3477c0097884edf31bd10
晨曦 <c15293452071@163.com>
2022-06-13 17:06:00 +0800

    token持久化储存

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,12 +1,13 @@
 /**
  * 存储用户信息,token、个人信息
  */
+import { getToken, setToken, removeToken } from '@/utils/auth.js'
 export default {
   // 模块化加锁
   namespaced: true,
   // 1.定义变量
   state: {
-    token: ''
+    token: getToken() || ' '
   },
   // 2.修改变量（同步）
   mutations: {
@@ -14,10 +15,13 @@ export default {
     setToken(state, token) {
       // 设置token
       state.token = token
+      // 持久化储存token(防止刷新丢token)
+      setToken(token)
     },
-    // 删除token
+    // 删除token(退出登录)
     delToken(state) {
       state.token = ''
+      removeToken()
     }
   },
   // 3.调用后台接口发送请求（异步）

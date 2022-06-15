abc0cce281f8040761175c38b6c27b8d6393b959
晨曦 <c15293452071@163.com>
2022-06-15 10:10:56 +0800

    添加路由前置守卫

--- src/permission.js
+++ src/permission.js
@@ -1,64 +1,29 @@
-// import router from './router'
-// import store from './store'
-// import { Message } from 'element-ui'
-// import NProgress from 'nprogress' // progress bar
-// import 'nprogress/nprogress.css' // progress bar style
-// import { getToken } from '@/utils/auth' // get token from cookie
-// import getPageTitle from '@/utils/get-page-title'
-//
-// NProgress.configure({ showSpinner: false }) // NProgress Configuration
-//
-// const whiteList = ['/login'] // no redirect whitelist
-//
-// router.beforeEach(async(to, from, next) => {
-//   // start progress bar
-//   NProgress.start()
-//
-//   // set page title
-//   document.title = getPageTitle(to.meta.title)
-//
-//   // determine whether the user has logged in
-//   const hasToken = getToken()
-//
-//   if (hasToken) {
-//     if (to.path === '/login') {
-//       // if is logged in, redirect to the home page
-//       next({ path: '/' })
-//       NProgress.done()
-//     } else {
-//       const hasGetUserInfo = store.getters.name
-//       if (hasGetUserInfo) {
-//         next()
-//       } else {
-//         try {
-//           // get user info
-//           await store.dispatch('user/getInfo')
-//
-//           next()
-//         } catch (error) {
-//           // remove token and go to login page to re-login
-//           await store.dispatch('user/resetToken')
-//           Message.error(error || 'Has Error')
-//           next(`/login?redirect=${to.path}`)
-//           NProgress.done()
-//         }
-//       }
-//     }
-//   } else {
-//     /* has no token*/
-//
-//     if (whiteList.indexOf(to.path) !== -1) {
-//       // in the free login whitelist, go directly
-//       next()
-//     } else {
-//       // other pages that do not have permission to access are redirected to the login page.
-//       next(`/login?redirect=${to.path}`)
-//       NProgress.done()
-//     }
-//   }
-// })
-//
-// router.afterEach(() => {
-//   // finish progress bar
-//   NProgress.done()
-// })
+// 导入路由实例
+import router from '@/router'
+// 导入vuex实例
+import store from '@/store'
+// 添加白名单
+const VIP = ['/login', '/404']
+// 添加路由前置守卫
+router.beforeEach((to, from, next) => {
+// 有token
+  if (store.getters.token) {
+    // 是登录页
+    if (to.path === '/login') {
+      // 避免重复登录
+      next('/')
+    } else {
+      // 不是登录页，放行
+      next()
+    }
+  } else {
+    // 没有token，是否在白名单
+    if (VIP.includes(to.path) > -1) {
+    // 在白名单放行
+      next()
+    } else {
+      // 不在白名单,跳到登录页
+      next('/login')
+    }
+  }
+})

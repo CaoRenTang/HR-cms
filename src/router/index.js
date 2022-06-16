ce990a0d3d37a0f8b6648362c0425c5c4e77e23c
晨曦 <c15293452071@163.com>
2022-06-16 15:36:56 +0800

    创建动态路由

--- src/router/index.js
+++ src/router/index.js
@@ -7,29 +7,13 @@ Vue.use(Router)
 import Layout from '@/layout'
 
 /**
- * Note: sub-menu only appear when route children.length >= 1
- * Detail see: https://panjiachen.github.io/vue-element-admin-site/guide/essentials/router-and-nav.html
- *
- * hidden: true                   if set true, item will not show in the sidebar(default is false)
- * alwaysShow: true               if set true, will always show the root menu
- *                                if not set alwaysShow, when item has more than one children route,
- *                                it will becomes nested mode, otherwise not show the root menu
- * redirect: noRedirect           if set noRedirect will no redirect in the breadcrumb
- * name:'router-name'             the name is used by <keep-alive> (must set!!!)
- * meta : {
-    roles: ['admin','editor']    control the page roles (you can set multiple roles)
-    title: 'title'               the name show in sidebar and breadcrumb (recommend set)
-    icon: 'svg-name'/'el-icon-x' the icon show in the sidebar
-    breadcrumb: false            if set false, the item will hidden in breadcrumb(default is true)
-    activeMenu: '/example/list'  if set path, the sidebar will highlight the path you set
-  }
- */
-
-/**
  * constantRoutes
  * a base page that does not have permission requirements
  * all roles can be accessed
  */
+// 动态路由：需要做权限控制的路由，用户如果权限不一致访问到的路由也不一样
+export const asyncRoutes = []
+// 静态路由：不需要做权限控制的路由，每个用户都可以正常访问
 export const constantRoutes = [
   {
     path: '/login',
@@ -60,9 +44,10 @@ export const constantRoutes = [
 ]
 
 const createRouter = () => new Router({
-  // mode: 'history', // require service support
+  // 控制路由滚动行为  滚动到顶部
   scrollBehavior: () => ({ y: 0 }),
-  routes: constantRoutes
+  // 组合到一起形成完成路由表
+  routes: [...constantRoutes, ...asyncRoutes]
 })
 
 const router = createRouter()

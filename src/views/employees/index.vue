5be7baf6e299eb73bf026d6dea7be15bf36c389a
晨曦 <c15293452071@163.com>
2022-06-19 16:24:01 +0800

    员工管理页面头部组件封装

--- src/views/employees/index.vue
+++ src/views/employees/index.vue
@@ -1,10 +1,32 @@
 <template>
-  <div>员工管理</div>
+  <div class="employees-container">
+    <div class="app-container">
+      <page-tools>
+        <!-- 插入到left插槽位置 -->
+        <template #left>
+          <span>本月: 社保在缴 公积金在缴</span>
+        </template>
+        <!-- 插入到right插槽位置 -->
+        <template #right>
+          <el-button type="warning">导入excel</el-button>
+          <el-button type="danger">导出excel</el-button>
+          <el-button type="primary">新增员工</el-button>
+        </template>
+      </page-tools>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
+import PageTools from '@/components/PageTools'
 
+export default {
+  components: {
+    PageTools
+  },
+  data() {
+    return {}
+  }
 }
 </script>
 

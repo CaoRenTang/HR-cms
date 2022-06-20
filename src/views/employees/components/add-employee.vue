55d24db1398c92d4d728eb40b5d76e544ef1f807
晨曦 <c15293452071@163.com>
2022-06-20 16:49:07 +0800

    员工管理->添加表单校验规则

--- src/views/employees/components/add-employee.vue
+++ src/views/employees/components/add-employee.vue
@@ -12,25 +12,25 @@
       :rules="rules"
     >
       <el-form-item label="姓名" prop="username">
-        <el-input v-model="formData.username" style="width:50%" placeholder="请输入姓名"/>
+        <el-input v-model="formData.username" style="width:60%" placeholder="请输入姓名"/>
       </el-form-item>
       <el-form-item label="手机" prop="mobile">
-        <el-input v-model="formData.mobile" style="width:50%" placeholder="请输入手机号"/>
+        <el-input v-model="formData.mobile" style="width:60%" placeholder="请输入手机号"/>
       </el-form-item>
       <el-form-item label="入职时间" prop="timeOfEntry">
-        <el-date-picker v-model="formData.timeOfEntry" style="width:50%" placeholder="请选择入职时间"/>
+        <el-date-picker v-model="formData.timeOfEntry" style="width:60%" placeholder="请选择入职时间"/>
       </el-form-item>
       <el-form-item label="聘用形式" prop="formOfEmployment">
-        <el-select v-model="formData.formOfEmployment" style="width:50%" placeholder="请选择" value=""/>
+        <el-select v-model="formData.formOfEmployment" style="width:60%" placeholder="请选择" value=""/>
       </el-form-item>
       <el-form-item label="工号" prop="workNumber">
-        <el-input v-model="formData.workNumber" style="width:50%" placeholder="请输入工号"/>
+        <el-input v-model="formData.workNumber" style="width:60%" placeholder="请输入工号"/>
       </el-form-item>
       <el-form-item label="部门" prop="departmentName">
-        <el-input v-model="formData.departmentName" style="width:50%" placeholder="请选择部门"/>
+        <el-input v-model="formData.departmentName" style="width:60%" placeholder="请选择部门"/>
       </el-form-item>
       <el-form-item label="转正时间" prop="correctionTime">
-        <el-date-picker v-model="formData.correctionTime" style="width:50%" placeholder="请选择转正时间"/>
+        <el-date-picker v-model="formData.correctionTime" style="width:60%" placeholder="请选择转正时间"/>
       </el-form-item>
     </el-form>
     <!-- footer插槽 -->
@@ -64,7 +64,28 @@ export default {
         correctionTime: '' // 员工转正日期
       },
       // 校验表单
-      rules: {}
+      rules: {
+        username: [
+          {required: true, message: '用户姓名不能为空', trigger: ['blur', 'change']},
+          {min: 1, max: 4, message: '用户姓名为1-4位', trigger: ['blur', 'change']}
+        ],
+        mobile: [
+          {required: true, message: '手机号不能为空', trigger: ['blur', 'change']},
+          {pattern: /^1[3-9]\d{9}$/, message: '手机号格式不正确', trigger: ['blur', 'change']}
+        ],
+        formOfEmployment: [
+          {required: true, message: '聘用形式不能为空', trigger: ['blur', 'change']}
+        ],
+        workNumber: [
+          {required: true, message: '工号不能为空', trigger: ['blur', 'change']}
+        ],
+        departmentName: [
+          {required: true, message: '部门不能为空', trigger: ['blur', 'change']}
+        ],
+        timeOfEntry: [
+          {required: true, message: '请选择入职时间', trigger: ['blur', 'change']}
+        ]
+      }
     }
   },
   methods: {
@@ -77,5 +98,4 @@ export default {
 </script>
 
 <style scoped>
-
 </style>

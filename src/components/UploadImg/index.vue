6802bccd9f96a6b8041b69b3d0d9408f714b1e20
晨曦 <c15293452071@163.com>
2022-06-22 16:47:49 +0800

    个人详情->云上传实现

--- src/components/UploadImg/index.vue
+++ src/components/UploadImg/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-<!--    `:http-request="upload" 开启自定义上传-->
+    <!--    `:http-request="upload" 开启自定义上传-->
     <el-upload
       :before-upload="beforeAvatarUpload"
       :http-request="upload"
@@ -16,6 +16,13 @@
 </template>
 
 <script>
+// 导入cos的SDK调用
+import COS from 'cos-js-sdk-v5'
+
+const cos = new COS({
+  SecretId: 'AKIDJSLzNVMyKPEn5rMaZHismQnunPkob9QR', // 身份识别ID
+  SecretKey: 'lGUR7WfZDaiZ9SVIp67rKWR6UbS2LOBi' // 身份秘钥
+})
 export default {
   data() {
     return {
@@ -28,7 +35,24 @@ export default {
     // },
     // 自定义上传
     upload(params) {
-      console.log(params)
+      // console.log(params)
+      // 云上传代码
+      cos.putObject({
+        Bucket: 'hr-cms-1311688877', /* 填入您自己的存储桶，必须字段 */
+        Region: 'ap-beijing', /* 存储桶所在地域，例如ap-beijing，必须字段 */
+        Key: '1.jpg', /* 存储在桶里的对象键（例如1.jpg，a/b/test.txt），必须字段 */
+        StorageClass: 'STANDARD',
+        Body: params.file, // 上传文件对象
+        onProgress: (progressData) => {
+          console.log(JSON.stringify(progressData))
+        }
+      }, (err, data) => {
+        // 上传失败或成功
+        console.log(err || data)
+        if (data.statusCode === 200) {
+          this.imageUrl = `https://${data.Location}`
+        }
+      })
     },
     beforeAvatarUpload(file) {
       const isJPG = file.type === 'image/jpeg'

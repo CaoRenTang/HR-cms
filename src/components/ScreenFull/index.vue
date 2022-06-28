2c728ffba6c826448cc365f7278bfb1bdb3ac114
晨曦 <c15293452071@163.com>
2022-06-28 10:41:51 +0800

    首页->全屏按钮图标切换

--- src/components/ScreenFull/index.vue
+++ src/components/ScreenFull/index.vue
@@ -1,25 +1,49 @@
 <template>
   <!-- 放置一个图标 -->
   <div>
-    <svg-icon class="fullscreen" icon-class="fullscreen" @click="toggleScreen"/>
+    <svg-icon
+      :icon-class="isFull? 'exit-fullscreen': 'fullscreen'"
+      class="fullscreen"
+      @click="toggleScreen"
+    />
   </div>
 </template>
 
 <script>
-import screenfull from 'screenfull'
+import sf from 'screenfull'
 
 export default {
   name: 'ScreenFull',
+  data() {
+    return {
+      isFull: false
+    }
+  },
+  // 监听全屏状态
+  created() {
+    if (sf.isEnabled) {
+      sf.on('change', () => {
+        if (sf.isFullscreen) {
+          this.isFull = true
+        } else {
+          this.isFull = false
+        }
+      })
+    }
+  },
+  beforeDestroy() {
+    sf.off('change')
+  },
   methods: {
     toggleScreen() {
       // 判断是否可用
-      if (!screenfull.isEnabled) {
+      if (!sf.isEnabled) {
         // 此时全屏不可用
         this.$message.warning('此时全屏组件不可用')
         return
       }
-      //   如果可用 就可以全屏
-      screenfull.toggle()
+      // 如果可用 就可以全屏
+      sf.toggle()
     }
   }
 }

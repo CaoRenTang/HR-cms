3bfe9bcf78559f6bcc095103bbd67ced3f03898a
晨曦 <c15293452071@163.com>
2022-06-28 10:06:39 +0800

    首页->全屏组件点击事件

--- src/components/ScreenFull/index.vue
+++ src/components/ScreenFull/index.vue
@@ -1,13 +1,27 @@
 <template>
   <!-- 放置一个图标 -->
   <div>
-    <svg-icon class="fullscreen" icon-class="fullscreen"/>
+    <svg-icon class="fullscreen" icon-class="fullscreen" @click="toggleScreen"/>
   </div>
 </template>
 
 <script>
+import screenfull from 'screenfull'
+
 export default {
-  name: 'ScreenFull'
+  name: 'ScreenFull',
+  methods: {
+    toggleScreen() {
+      // 判断是否可用
+      if (!screenfull.isEnabled) {
+        // 此时全屏不可用
+        this.$message.warning('此时全屏组件不可用')
+        return
+      }
+      //   如果可用 就可以全屏
+      screenfull.toggle()
+    }
+  }
 }
 </script>
 
